b738d42738177168442224be1b5fac57191169ab
MarveyVoleHunter <anthony.thompson@maxcontact.com>
2026-06-28 17:28:04 +0100

    Added discount

--- OrderProcessing.Api/Controllers/CustomersController.cs
+++ OrderProcessing.Api/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using OrderProcessing.DataStore;
+using OrderProcessing.DiscountService;
 using OrderProcessing.Domain.Entities;
 
 namespace OrderProcessing.Api.Controllers;
@@ -9,10 +10,12 @@ namespace OrderProcessing.Api.Controllers;
 public class CustomersController : ControllerBase
 {
     private readonly IRepository _repository;
+    private readonly DiscountCalculator _discountCalculator;
 
-    public CustomersController(IRepository repository)
+    public CustomersController(IRepository repository, DiscountCalculator discountCalculator)
     {
         _repository = repository;
+        _discountCalculator = discountCalculator;
     }
 
     [HttpGet]
@@ -26,5 +29,20 @@ public class CustomersController : ControllerBase
     {
         var customer = _repository.GetCustomerById(id);
         return customer is not null ? Ok(customer) : NotFound();
+    }
+
+    [HttpPatch("{id:int}/ApplyDiscount")]
+    public ActionResult<Customer> ApplyDiscount(int id)
+    {
+        var customer = _repository.GetCustomerById(id);
+
+        if (customer is null)
+        {
+            return NotFound();
+        }
+
+        _discountCalculator.ApplyDiscountsToCustomer(customer);
+
+        return Ok(customer);
     }
 }
--- OrderProcessing.Api/Program.cs
+++ OrderProcessing.Api/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OrderProcessing.DataStore;
+using OrderProcessing.DiscountService;
+using OrderProcessing.DiscountService.DiscountRules;
+using OrderProcessing.ImportService;
+using OrderProcessing.ImportService.CsvReaders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +15,15 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IRepository, Repository>();
 
+ConfigureServices(builder.Services);
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var importer = scope.ServiceProvider.GetRequiredService<DataImporter>();
+    importer.DoImports();
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -24,3 +36,25 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void ConfigureServices(IServiceCollection services)
+{
+    var discountRules = new List<IDiscountRule>
+    {
+        new CaliforniaDiscountRule(),
+    };
+
+    services.AddAutoMapper(cfg => { cfg.AddProfile<ImportMappingProfile>(); });
+    services.AddLogging(builder => builder
+        .AddFilter("Microsoft", LogLevel.Warning)
+        .AddFilter("System", LogLevel.Warning)
+        .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+        .AddConsole());
+    services.AddSingleton<CustomerCsvReader>();
+    services.AddSingleton<OrderCsvReader>();
+    services.AddSingleton<OrderItemCsvReader>();
+    services.AddSingleton<IEnumerable<IDiscountRule>>(discountRules);
+    services.AddSingleton<DataImporter>();
+    services.AddSingleton<DiscountCalculator>();
+    services.AddSingleton<IRepository, Repository>();
+}
